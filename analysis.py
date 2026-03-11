cd367d2a2a95f5a1efeb41f1d2165e0178779c97
Ursula113355 <Ursula113355@gmail.com>
2026-03-11 21:13:59 +0800

    add visualization

--- analysis.py
+++ analysis.py
@@ -12,6 +12,9 @@ GROUP BY city
 df = pd.read_sql_query(query, conn)
 print(df)
 
+df.plot(x="city", y="avg_purchase", kind="bar", title="Average Purchase by city").get_figure().savefig("Average Purchase by city.png", bbox_inches="tight")
+
+
 # 找最高消费城市
 top_city = df.sort_values("avg_purchase", ascending=False).iloc[0]
 print("\nCity with highest average purchase: ")
